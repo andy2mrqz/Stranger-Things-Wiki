f246ee050d29079ea9d60de0d7fe6fea4d5e8b8b
Evan McDaniel <eamigo13@gmail.com>
2017-11-02 23:15:17 -0600

    Total changes up to this point include the following:
    Update to entity framework 6.2.0
    
    Add Character model
    
    Add connection string to azure db in .webconfig file
    
    Added the DAL folder and the StrangerThingsContext.cs file
    
    Used scaffolding to add the CharactersController and all the Characters views (create, delete, details, edit, and index)
    
    Changed the character button on the home page to go to the Characters index view

---
+++ StrangerThings/StrangerThings/Controllers/CharactersController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StrangerThings.DAL;
+using StrangerThings.Models;
+
+namespace StrangerThings.Controllers
+{
+    public class CharactersController : Controller
+    {
+        private StrangerThingsContext db = new StrangerThingsContext();
+
+        // GET: Characters
+        public ActionResult Index()
+        {
+            return View(db.Characters.ToList());
+        }
+
+        // GET: Characters/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Character character = db.Characters.Find(id);
+            if (character == null)
+            {
+                return HttpNotFound();
+            }
+            return View(character);
+        }
+
+        // GET: Characters/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Characters/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CharacterID,CharacterFirstName,CharacterLastName,CharacterDescription,Hometown,YearOfBirth,PhotoLocation,Actor,Gender,Occupation,Quote")] Character character)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Characters.Add(character);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(character);
+        }
+
+        // GET: Characters/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Character character = db.Characters.Find(id);
+            if (character == null)
+            {
+                return HttpNotFound();
+            }
+            return View(character);
+        }
+
+        // POST: Characters/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CharacterID,CharacterFirstName,CharacterLastName,CharacterDescription,Hometown,YearOfBirth,PhotoLocation,Actor,Gender,Occupation,Quote")] Character character)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(character).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(character);
+        }
+
+        // GET: Characters/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Character character = db.Characters.Find(id);
+            if (character == null)
+            {
+                return HttpNotFound();
+            }
+            return View(character);
+        }
+
+        // POST: Characters/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Character character = db.Characters.Find(id);
+            db.Characters.Remove(character);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
