d30aa7d44b8d8904e5176e2d1085a5747fade05c
Evan McDaniel <eamigo13@gmail.com>
2017-11-02 22:24:20 -0600

    Updated to Entity Framework 6.2.0

---
+++ StrangerThings/StrangerThings/DAL/StrangerThingsContext.cs
@@ -0,0 +1,20 @@
+﻿using StrangerThings.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace StrangerThings.DAL
+{
+    public class StrangerThingsContext : DbContext
+    {
+        public StrangerThingsContext() : base("StrangerThingsContext")
+        {
+
+        }
+
+        //Create the DbSets for the different models
+        public DbSet<Character> Characters { get; set; }
+    }
+}
